2db60b8319738b4e5b7d88ff8f1689dda3b68d86
Tsinana <hlebusru@gmail.com>
2023-03-27 23:02:02 +0300

    task2_2 done

--- ruby_not_rwby/lab1/task2/task2.rb
+++ ruby_not_rwby/lab1/task2/task2.rb
@@ -5,8 +5,26 @@ def is_simple_digit(digit)
   true
 end
 
+
 def max_simple_div(digit)
-  digit.step(1,-1) {|i| if digit % i == 0 and is_simple_digit(i) then return i end}
+  digit.step(1,-1) {|i| return i if digit % i == 0 and is_simple_digit(i)}
 end
 
-puts "#{max_simple_div(-8)}"
+
+# def mult_numbers_div_5(digit)
+#   mult = 1
+#   digit.to_s.chars.select{|a| a.to_i % 5 != 0}.each{|b| mult*=b.to_i}
+#   mult
+# end 
+
+
+def mult_numbers_div_5(digit)
+  mult = 1
+  digit.digits.each{|a| mult*=a if a % 5 != 0 }
+  mult
+end
+
+
+puts "#{max_simple_div(10)}"
+puts "#{mult_numbers_div_5(1256)}"
+

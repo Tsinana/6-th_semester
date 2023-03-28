4e34fef1cd182128846ffb79f83af79058d4a9de
Tsinana <hlebusru@gmail.com>
2023-03-28 18:28:29 +0300

    task2 lvl up

--- ruby_not_rwby/lab1/task2/task2.rb
+++ ruby_not_rwby/lab1/task2/task2.rb
@@ -13,18 +13,21 @@ def max_div_with_condition(digit,condition = lambda { |i| true })
 end
 
 
-
 # Метод нахождения максимального простого делителя числа
 def max_simple_div(digit)
   digit.step(1,-1) {|i| return i if digit % i == 0 and is_simple_digit(i)}
 end
 
 
+# Метод нахождения произведение цифр числа
+def mult_numbers(digit)
+  digit.digits.reduce(:*)
+end
+
+
 # Метод нахождения произведение цифр числа c заданным условием
 def mult_numbers_with_condition(digit,condition = lambda { |num| true })
-  mult = 1
-  digit.digits.each{|num| mult*=num if condition.call(num)}
-  mult
+  digit.digits.reduce(1){|mult,num| mult*=num if condition.call(num)}
 end
 
 
@@ -40,6 +43,7 @@ def euclidean_algorithm(digit_a,digit_b)
   return digit_b if digit_a % digit_b == 0
   return euclidean_algorithm(digit_b,digit_a % digit_b)
 end
+
 
 # Метод нахождения НОД максимального нечетного непростого делителя 
 # числа и произведения цифр данного числа

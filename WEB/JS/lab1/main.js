af5d2a1667c53a0fa8d7b1843629beaf6efd5abe
Tsinana <hlebusru@gmail.com>
2023-04-04 18:24:38 +0300

    biba

--- WEB/JS/lab1/main.js
+++ WEB/JS/lab1/main.js
@@ -4,6 +4,25 @@ import { createCalendar } from "./components/calender/calendar.js";
 const GET_DATA_TIME = (a) => dateTime(a);
 const GET_CALENDAR = (a) => createCalendar(a);
 
-// setInterval(GET_DATA_TIME("IDDateTime"), 250);
-GET_DATA_TIME("dateTimeBtn");
-GET_CALENDAR("calenderBtn");
+let a = 0;
+dateTimeBtn.addEventListener("click", () => {
+  if (a == 0) {
+    GET_DATA_TIME("dateTimeBtn");
+    // setInterval(GET_DATA_TIME("dateTimeBtn"), 250);
+    a += 1;
+  } else {
+    document.getElementById("dateTimeBtn").nextSibling.remove();
+    a -= 1;
+  }
+});
+
+let a1 = 0;
+calenderBtn.addEventListener("click", () => {
+  if (a1 == 0) {
+    GET_CALENDAR("calenderBtn");
+    a1 += 1;
+  } else {
+    document.getElementById("calenderBtn").nextSibling.remove();
+    a1 -= 1;
+  }
+});

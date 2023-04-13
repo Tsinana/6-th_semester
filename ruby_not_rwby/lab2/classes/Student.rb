dcce6d4ae3c3684ed6906ad750bebacbbb931898
Tsinana <hlebusru@gmail.com>
2023-04-13 18:19:56 +0300

    lab2 - almost my attr

--- ruby_not_rwby/lab2/classes/Student.rb
+++ ruby_not_rwby/lab2/classes/Student.rb
@@ -1,40 +1,57 @@
 require './classes/MyErrors'
+require './modules/AttrValidated'
 
 class Student
-	attr_accessor :id, :surname, :name, :patronymic, :telegram, :email, :git
-	attr_reader :phone_number
+	include AttrValidated 
+
+	ID_regex = /^[0-9]+$/
+	Phone_number_regex = /^[(\+7)8][0-9]+$/
+	Full_name_regex = /^[\w]+$/
+	Telegram_regex = /^@[\w0-9]+$/
+	Git_regex = /^[\w0-9]+$/
+	Email_reget = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+	# attr_accessor :id, :surname, :name, :patronymic, :telegram, :email, :git ,:phone_number
+	attr_validated (:id) {|val| val =~ id || val.nil?}
+	attr_validated ((:surname,:name,:patronymic)) {|val| val =~ Full_name_regex || val.nil?} 
+	attr_validated (:telegram) {|val| val =~ Telegram_regex || val.nil?} 
+	attr_validated (:email) {|val| val =~ Phone_number_regex || val.nil?} 
+	attr_validated (:git) {|val| val =~ Git_regex || val.nil?} 
+	attr_validated (:phone_number) {|val| val =~ Phone_number_regex || val.nil?} 
+
 	def initialize(id: , surname: , name: , patronymic: ,phone_number: nil, telegram: nil, email: nil, git: nil)
-		puts Student::phone_number_check(phone_number,id)
-		@id = id 
-		@surname = surname
-		@name = name 
-		@patronymic = patronymic
-		@telegram = telegram
-		@email = email 
-		@git = git
-		@phone_number = phone_number
+		self.id = id 
+		self.surname = surname
+		self.name = name 
+		self.patronymic = patronymic
+		self.telegram = telegram
+		self.email = email 
+		self.git = git
+		self.phone_number = phone_number
 	end
 
-	# title:value; ; - separator
-	def to_s
-		"id:#{@id};surname:#{@surname};name:#{@name};patronymic:#{@patronymic};phone_number:#{@phone_number};telegram:#{@telegram};email:#{@email};git:#{@git};"
+
+	# функция преобразования объекта в строку с разделителем арг: -separator 
+	def to_s(separator = ';')
+		"#{@id}#{separator}#{@surname}#{separator}#{@name}#{separator}#{@patronymic}#{separator}#{@phone_number}#{separator}#{@telegram}#{separator}#{@email}#{separator}#{@git}#{separator}"
 	end
 
-	def phone_number=(val)
-		@phone_number = val
+
+	# функция вывода титулов объекта в строку с разделителем арг: -separator
+	def print_title(separator = ';')
+		"id#{separator}surnamename#{separator}patronymic#{separator}phone_number#{separator}telegram#{separator}email#{separator}git#{separator}"
 	end
 
-	def set_phone_number(val)
-		@phone_number = val
-	end
 
-	def self.phone_number_check(phone_number, id = nil)
-		raise UncorrectPhoneError , "id: #{id} phone_number: #{phone_number}" if !phone_number.nil? && !Student::phone_number_validation?(phone_number)
-		true
-	end
+	# # функция проверяет корректность номера. арг: +номер_телефона -id, рез: true, искл: UncorrectPhoneError
+	# def self.phone_number_check(phone_number, id = nil)
+	# 	raise UncorrectPhoneError , "id: #{id} phone_number: #{phone_number}" if !phone_number.nil? && !Student::phone_number_validation?(phone_number)
+	# 	true
+	# end
 
-	# Проверка номера 
-	def self.phone_number_validation?(phone_number)
-		phone_number =~ /^[(\+7)8][0-9]{11}$/
+
+	# # функция проверяет, является ли строка номером телефона. арг: +номер_телефона, рез: bool
+	# def self.phone_number_validation?(phone_number)
+	# 	phone_number =~ /^[(\+7)8][0-9]{11}$/
 	end
 end
--- ruby_not_rwby/lab2/main.rb
+++ ruby_not_rwby/lab2/main.rb
@@ -6,8 +6,8 @@ student << Student.new(id: 1, surname: 'lupov', name: 'lupa', patronymic: 'lupov
 student << Student.new(id: 2, surname: 'pupov', name: 'pupa', patronymic: 'pupovich', phone_number: '+79002112332', telegram: '@pupa', email: 'pupa@mail.ru', git: 'pupaProgramer')
 #Валидации же нет! Вот и всякие иванушки появляются
 student << Student.new(id: 3, surname: 'Ivanov', name: 'Ivan', patronymic: 'Ivanich', telegram: 'ul. Pushkina', email: 'ivan_boitsia_@s')
-student.each{|person| puts "\n"+person.to_s }
-
+student.each{|person| puts "\n"+person.to_s(' ') }
+student[0].phone_number = "\+79002442662"
 rescue StandardError => e
 	puts "Error: #{e.message}"
 	

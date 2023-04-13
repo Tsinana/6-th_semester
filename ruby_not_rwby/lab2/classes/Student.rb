2b5bf0f406e640f2579bec2870b115348d7cf04f
Tsinana <hlebusru@gmail.com>
2023-04-13 18:52:10 +0300

    Update Student.rb

--- ruby_not_rwby/lab2/classes/Student.rb
+++ ruby_not_rwby/lab2/classes/Student.rb
@@ -9,13 +9,12 @@ class Student
 	Full_name_regex = /^[\w]+$/
 	Telegram_regex = /^@[\w0-9]+$/
 	Git_regex = /^[\w0-9]+$/
-	Email_reget = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+	Email_regex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
-	# attr_accessor :id, :surname, :name, :patronymic, :telegram, :email, :git ,:phone_number
-	attr_validated (:id) {|val| val =~ id || val.nil?}
-	attr_validated ((:surname,:name,:patronymic)) {|val| val =~ Full_name_regex || val.nil?} 
+	attr_validated (:id) {|val| val =~ ID_regex || val.nil?}
+	attr_validated :surname, :name, :patronymic  do |val| val =~ Full_name_regex || val.nil? end
 	attr_validated (:telegram) {|val| val =~ Telegram_regex || val.nil?} 
-	attr_validated (:email) {|val| val =~ Phone_number_regex || val.nil?} 
+	attr_validated (:email) {|val| val =~ Email_regex || val.nil?} 
 	attr_validated (:git) {|val| val =~ Git_regex || val.nil?} 
 	attr_validated (:phone_number) {|val| val =~ Phone_number_regex || val.nil?} 
 
@@ -40,18 +39,5 @@ class Student
 	# функция вывода титулов объекта в строку с разделителем арг: -separator
 	def print_title(separator = ';')
 		"id#{separator}surnamename#{separator}patronymic#{separator}phone_number#{separator}telegram#{separator}email#{separator}git#{separator}"
-	end
-
-
-	# # функция проверяет корректность номера. арг: +номер_телефона -id, рез: true, искл: UncorrectPhoneError
-	# def self.phone_number_check(phone_number, id = nil)
-	# 	raise UncorrectPhoneError , "id: #{id} phone_number: #{phone_number}" if !phone_number.nil? && !Student::phone_number_validation?(phone_number)
-	# 	true
-	# end
-
-
-	# # функция проверяет, является ли строка номером телефона. арг: +номер_телефона, рез: bool
-	# def self.phone_number_validation?(phone_number)
-	# 	phone_number =~ /^[(\+7)8][0-9]{11}$/
 	end
 end

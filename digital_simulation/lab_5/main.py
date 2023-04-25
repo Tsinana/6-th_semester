3a352c83f19147a2754e26e0b138083e59696f56
Tsinana <hlebusru@gmail.com>
2023-04-26 02:53:23 +0300

    lab5's done

--- digital_simulation/lab_5/main.py
+++ digital_simulation/lab_5/main.py
@@ -1,93 +1,109 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-from collections import deque
 from prettytable import PrettyTable
 
 
-def plot(a, b, c):
-  plt.plot(a, color='red',
-           label='Все время станка', )
+class Machine:
+  def __init__(self):
+    self.t_to_break = self.generate_repair_cooldown()
+    self.t_work = 0
+    self.t_set_up = 0
+    self.t_repair = 0
+    self.t_lost = 0
+    self.c_items_done = 0
+    self.c_breaks = 0
 
-  plt.plot(b, color='orange',
-           label='Работа', )
+  # Выполнить одну деталь - учет времени производства и проверка на поломку
+  def do_work(self):
+    set_up = self.generate_set_up_time()
+    work = self.generate_task_execution_time()
+    self.t_to_break -= set_up + work
 
-  plt.plot(c, color='green',
-           label='Починка, отладка и интервалы')
-
-  plt.xlabel('Количество сделанных деталей')
-  plt.ylabel('Время работы станка')
-
-  # naming the title of the plot
-  plt.title('График работы станка')
-  # Тут что-то придумать с отрисовкой надо. Пусть красиво смотриться
-  plt.legend()
-  plt.show()
-
-
-def table_draw(td, th, columns):
-  table = PrettyTable(th)
-  while td:
-    # Используя срез добавляем первые columns элементa в строку.
-    table.add_row(td[:columns])
-    # Используя срез переопределяем td так, чтобы он
-    # больше не содержал первых columns элементов.
-    td = td[columns:]
-
-  print("{}".format(table))  # Печатаем таблицу
-
-
-def machine_work(details=500):
-  cooldown, current, counter = repair_cooldown(), 0, 0
-  time_all, time_work, time_chill = 0, 0, 0
-  in_progress = deque([i for i in range(details + 1)])
-
-  while len(in_progress) != 0:
-    set_up = setting_up()
-    val = intervals()
-    task = task_execution()
-    cooldown -= current
-
-    if cooldown <= 0:
-      cooldown = repair_cooldown()
-      work = repair()
-
-      time_chill += work
-      time_all += work
-      counter += 1
-      current = 0
-      in_progress.rotate(-1)
-
+    if self.t_to_break < 0:
+      self.repair_machin(set_up, work)
     else:
-      time_work += set_up + task
-      time_chill += val
-      time_all += set_up + val + task
-      current = round(set_up + val + task, 2)
+      self.t_work += work
+      self.t_set_up += set_up
+      self.c_items_done += 1
 
-      in_progress.pop()
+  # Ремонт машины - установка нового времени до поломки и учет потерянного времени с вызов do_work()
+  def repair_machin(self, set_up, work):
+    repair = self.generate_repair_time()
+    self.t_repair += repair
+    self.t_lost += set_up + work + repair
+    self.t_to_break = self.generate_repair_cooldown()
+    self.c_breaks += 1
+    self.do_work()
 
-  return round(time_all, 2), round(time_work, 2), round(time_chill, 2), counter
+  # Получить все поля машины
+  def get_status(self):
+    return [round(self.t_work + self.t_set_up, 4), round(self.t_work, 4), round(self.t_set_up, 4),
+            round(self.t_lost, 4), round(self.t_lost - self.t_repair, 4), round(self.t_repair, 4),
+            round(self.c_items_done, 4), round(self.c_breaks, 4)]
+
+  # Генератор случайного времени выполнения задачи
+  @staticmethod
+  def generate_task_execution_time():
+    return np.random.normal(loc=0.5, scale=0.1)
+
+  # Генератор времени на починку
+  @staticmethod
+  def generate_repair_cooldown():
+    return np.random.normal(loc=20, scale=2)
+
+  # Генератор времени на починку
+  @staticmethod
+  def generate_repair_time():
+    return np.random.uniform(low=0.1, high=0.5)
+
+  # Генератор времени на наладку
+  @staticmethod
+  def generate_set_up_time():
+    return np.random.uniform(low=0.2, high=0.5)
 
 
-def machine_operation():
-  # Экспоненциальное распределение
-  intervals = lambda: np.random.exponential()  # Интервалы работы
-  # Нормальное распределение
-  task_execution = lambda: np.random.normal(loc=0.5, scale=0.1)  # Выполнения детали
-  repair_cooldown = lambda: np.random.normal(loc=20, scale=2)  # кд на починку
-  # Равномерное распределение
-  repair = lambda: np.random.uniform(low=0.1, high=0.5)  # Время починки
-  setting_up = lambda: np.random.uniform(low=0.2, high=0.5)  # Наладка перед выполнением задания
-  work_time = 0
-  simply_time = 0
+# Генератор случайных интервалов времени между задачами
+def generate_interval():
+  return np.random.exponential()
 
-  for detail in range(0, 501):
-    # Время насттройки это полезное действие?
 
-    simply_time += setting_up()
-    work_time += task_execution()
-    simply_time += intervals()
+def print_table(data, field_names):
+  table = PrettyTable()
+  table.field_names = field_names
+  table.add_rows(data)
+  print(table)
+
+
+def model_machine(count_machine, count_details):
+  data = []
+  for _ in range(count_machine):
+    my_machine = Machine()
+    waiting = 0
+
+    old_status = my_machine.get_status()
+    my_machine.do_work()
+    new_status = my_machine.get_status()
+    runtime = new_status[0] + new_status[1] + new_status[3] - old_status[0] - old_status[1] - old_status[3]
+
+    for _ in range(count_details):
+      interval = generate_interval()
+      waiting = round(interval - runtime, 4)
+
+      old_status = new_status
+      my_machine.do_work()
+      new_status = my_machine.get_status()
+      runtime = new_status[0] + new_status[1] + new_status[3] - old_status[0] - old_status[1] - old_status[3]
+
+    status_machine = my_machine.get_status()
+    status_machine.append(waiting)
+    data.append(status_machine)
+
+  field_names = ["Работа станка", "Выполнение детали", "Настройка станка", "Игра в бирюльки", "Утрата работы",
+                 "Ремонт станка", "Сделано деталей", "Поломок", "Простой"]
+  print_table(data, field_names)
 
 
 if __name__ == '__main__':
-  print(np.random.exponential())
+  count_machine = int(input("Введите количество моделей: "))
+  count_details = int(input("Введите объем работ для каждой модели: "))
+
+  model_machine(count_machine, count_details)
